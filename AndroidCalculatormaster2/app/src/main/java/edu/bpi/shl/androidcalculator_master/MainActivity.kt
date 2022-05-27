a70de9648fbc9d9f0ae5f1d243579203e26faa78
“LAKELAKY” <295117392@qq.com>
2022-05-27 10:22:55 +0800

    提交信息

---
+++ AndroidCalculatormaster2/app/src/main/java/edu/bpi/shl/androidcalculator_master/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.bpi.shl.androidcalculator_master
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
